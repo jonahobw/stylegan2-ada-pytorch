a436eb8f0e3a53e9babdb05581ae25a26ddd7730
jonahobw <jgow98@gmail.com>
2022-11-08 22:16:28 -0500

    bug fixes

--- model_inversion.py
+++ model_inversion.py
@@ -12,6 +12,7 @@ import os
 from typing import List, Optional
 
 import click
+from tqdm import tqdm
 import dnnlib
 import numpy as np
 import PIL.Image
@@ -22,33 +23,11 @@ from torchvision.models import resnet50, ResNet50_Weights
 import legacy
 
 
-def size_range(s: str) -> List[int]:
-    """Accept a range 'a-c' and return as a list of 2 ints."""
-    return [int(v) for v in s.split("-")][::-1]
-
-
-# ----------------------------------------------------------------------------
-
-
-def load_gan(gan_pkl: str, scale_type: str, size: List[int], device: str):
-    # custom size code from https://github.com/eps696/stylegan2ada/blob/master/src/_genSGAN2.py
-    if size:
-        size = size_range(size)
-        print("render custom size: ", size)
-        print("padding method:", scale_type)
-        custom = True
-    else:
-        custom = False
-
-    G_kwargs = dnnlib.EasyDict()
-    G_kwargs.size = size
-    G_kwargs.scale_type = scale_type
-
+def load_gan(gan_pkl: str, device: str):
     print('Loading networks from "%s"...' % gan_pkl)
     device = torch.device(device)
     with dnnlib.util.open_url(gan_pkl) as f:
-        G = legacy.load_network_pkl(f, custom=custom, **G_kwargs)["G_ema"].to(device)  # type: ignore
-
+        G = legacy.load_network_pkl(f)["G_ema"].to(device)  # type: ignore
     return G
 
 
@@ -64,45 +43,44 @@ def generate_image(
 ):
     """Generate images using pretrained network pickle."""
 
-    if outdir is None:
-        ctx.fail("--outdir option is required")
     device = torch.device(device)
 
     os.makedirs(outdir, exist_ok=True)
     label = torch.zeros([1, G.c_dim], device=device)
 
     # Generate image
-    print("Generating image for seed %d" % (seed))
     z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
     noise = "const"
     if latent is not None:
         if not isinstance(latent, torch.Tensor):
-            z = torch.from_numpy(latent).to(device)
+            z = torch.from_numpy(latent).unsqueeze(dim=0).to(device)
         else:
-            z = latent.to(device)
+            z = latent.unsqueeze(dim=0).to(device)
         noise = "none"
     img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise)
     img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
     img_view = PIL.Image.fromarray(img[0].cpu().numpy(), "RGB")
+    if save is not None or show:
+        print("Generating image for seed %d" % (seed))
     if save is not None:
         img_view.save(f"{outdir}/seed{save}.png")
     if show:
         img_view.show()
-    return img
+    return img, z.squeeze(dim=0)
 
 
 def create_f(target_class, classifier, preprocess, gan, outdir, truncation_psi, device):
-    def f(x, save=None, show=False):
-        pixels = generate_image(
+    def f(z, save=None, show=False):
+        pixels, z = generate_image(
             G=gan,
             outdir=outdir,
             truncation_psi=truncation_psi,
             device=device,
-            latent=x,
+            latent=z,
             save=save,
             show=show,
         )
-        input = preprocess(pixels.unsqueeze(dim=0))
+        input = preprocess(torch.permute(pixels, (0, 3, 1, 2))).to(device)
         # todo format pixels for input to classifier
         output = classifier(input)
         output = F.softmax(output[0], dim=0)
@@ -124,16 +102,14 @@ def load_classifier(classifier_pkl: str, device: str):
     return model, preprocess
 
 
-def num_grad(f, x, delta=1):
-    # todo preprocess x to make it 1D
-    grad = np.zeros(len(x))
-    a = np.copy(x)
-    for i in range(len(x)):
-        a[i] = x[i] + delta
-        # todo postprocess a to make it ?
-        grad[i] = (f(a) - f(x)) / delta
+def num_grad(f, z, delta=1):
+    grad = torch.zeros(len(z), device=z.device)
+    a = z.detach().clone()
+    for i in tqdm(range(len(z))):
+        a[i] = z[i] + delta
+        grad[i] = (f(a) - f(z)) / delta
         a[i] -= delta
-    return x
+    return grad
 
 
 @click.command()
@@ -156,15 +132,6 @@ def num_grad(f, x, delta=1):
 )
 @click.option("--outdir", type=str, help="Where to store generated images")
 @click.option(
-    "--scale-type",
-    type=click.Choice(["pad", "padside", "symm", "symmside"]),
-    default="pad",
-    help="scaling method for --size",
-    required=False,
-    show_default=True,
-)
-@click.option("--size", type=size_range, help="size of output (in format x-y)")
-@click.option(
     "--seed", type=int, help="List of random seeds", default=0, show_default=True
 )
 @click.option(
@@ -184,23 +151,29 @@ def num_grad(f, x, delta=1):
     default=0,
     show_default=True,
 )
+@click.option(
+    "--step_size",
+    type=float,
+    help="Step size for gradient ascent",
+    default=2,
+    show_default=True,
+)
 def model_inversion(
     ctx: click.Context,
     gan_pkl: Optional[str],
     classifier_pkl: Optional[str],
     truncation_psi: Optional[float],
     outdir: Optional[str],
-    scale_type: Optional[str],
-    size: Optional[size_range],
     seed: Optional[int],
     count_max: int,
     device: Optional[str],
     target_class: Optional[int],
+    step_size: Optional[float]
 ):
     classifier, preprocess = load_classifier(
         classifier_pkl=classifier_pkl, device=device
     )
-    gan = load_gan(gan_pkl=gan_pkl, scale_type=scale_type, size=size, device=device)
+    gan = load_gan(gan_pkl=gan_pkl, device=device)
     f = create_f(
         target_class=target_class,
         classifier=classifier,
@@ -212,16 +185,16 @@ def model_inversion(
     )
 
     # numeric black box gradient ascent
-    x = generate_image(
+    x, z = generate_image(
         G=gan, outdir=outdir, truncation_psi=truncation_psi, seed=seed, device=device
     )
     count = 0
     conf = 0
     while conf < 0.4 or count > count_max:
-        conf = f(x, save=str(count), show=True)
+        conf = f(z, save=str(count), show=True)
         print(f"Iteration {count}, Conf = {conf}")
-        grad = num_grad(f, x)
-        x += grad
+        grad = num_grad(f, z)
+        z += step_size*grad
         count += 1
 
     print(f"Model inversion completed in {count} iterations.")
